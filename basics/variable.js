2028c8a3d5eee886d927bc24d06be32e56e32af9
AnushkaGupta29 <anushka.gupta2901@gmail.com>
2025-11-29 03:09:30 +0000

    variable done

---
+++ basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 145225;
+let accountEmail = "anuska@gmail.com";
+var accountPassword = "1234";
+accountName = "Anushka";
+
+accountEmail= "yashi@gmail.com";
+accountPassword= "4567";
+
+/*
+ var can not be used in modern javascript because it is functional scope and it 
+ can be accessible outside the block which provide bug that's why always use let and const.
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassword, accountName]);
