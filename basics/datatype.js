01336403314a8332a906c1d993ef62e7c408afda
AnushkaGupta29 <anushka.gupta2901@gmail.com>
2025-11-30 02:43:46 +0000

    datatype done

---
+++ basics/datatype.js
@@ -0,0 +1,22 @@
+let name = "Anushka"
+let age  = 23;
+let isValidForVote = true;
+
+// console.log(name); // In javascript name is depricated because browser have already built-
+ // in property window.name which can create conflict, unexpected bugs and security issue.
+
+ console.log(age);
+
+ // String
+ // number
+ // bigint => for large number used in trading 
+ // boolean 
+ // undefined => 
+ // null => standalone value 
+ // symbol => unique
+
+ //object
+
+ console.log(typeof "hiii");  // String
+ console.log(typeof null);   // object
+ console.log(typeof undefined);  // undefined
